a65510e6f283273086c73db0928d7ee6c17a405d
Ruan Correia <correia.ruan@gmail.com>
2024-10-07 00:42:27 -0300

    Adicionado class beneficiario e opçao de crud para a class

--- crud/urls.py
+++ crud/urls.py
@@ -1,6 +1,10 @@
 from django.contrib import admin
 from django.urls import path, include
-from .views import home, salvar,editar, update, delete
+from .views import (
+    home, salvar, editar, update, delete,
+    lista_beneficiarios, salvar_beneficiario, editar_beneficiario,
+    update_beneficiario, delete_beneficiario
+)
 
 urlpatterns = [
     path('', home),
@@ -9,6 +13,12 @@ urlpatterns = [
     path('update/<int:id>', update, name='update'),
     path('delete/<int:id>', delete, name='delete'),
     
-
+# Beneficiarios
+    path('beneficiarios/', lista_beneficiarios, name='lista_beneficiarios'),
+    path('beneficiarios/salvar/', salvar_beneficiario, name='salvar_beneficiario'),
+    path('beneficiarios/editar/<int:id>/', editar_beneficiario, name='editar_beneficiario'),
+    path('beneficiarios/update/<int:id>/', update_beneficiario, name='update_beneficiario'),
+    path('beneficiarios/delete/<int:id>/', delete_beneficiario, name='delete_beneficiario'),
 ]
+
 
--- crud/views.py
+++ crud/views.py
@@ -1,5 +1,5 @@
-from django.shortcuts import render, redirect
-from .models import Pessoa
+from django.shortcuts import render, redirect, get_object_or_404
+from .models import Pessoa, Beneficiario
 
 def home(request):
     pessoas = Pessoa.objects.all
@@ -26,3 +26,41 @@ def delete(request, id):
     pessoa = Pessoa.objects.get(id=id)
     pessoa.delete()
     return redirect(home)
+
+# beneficiarios
+def lista_beneficiarios(request):
+    beneficiarios = Beneficiario.objects.all()
+    return render(request, "lista_beneficiarios.html", {"beneficiarios": beneficiarios})
+
+def salvar_beneficiario(request):
+    if request.method == "POST":
+        nome = request.POST.get("nome")
+        apelido = request.POST.get("apelido")
+        documento = request.POST.get("documento")
+        endereco = request.POST.get("endereco")
+        telefone = request.POST.get("telefone")
+        observacoes = request.POST.get("observacoes")
+        Beneficiario.objects.create(nome=nome, apelido=apelido, documento=documento, endereco=endereco, telefone=telefone, observacoes=observacoes)
+        return redirect(lista_beneficiarios)
+    return render(request, "form_beneficiario.html")
+
+def editar_beneficiario(request, id):
+    beneficiario = get_object_or_404(Beneficiario, id=id)
+    return render(request, "update_beneficiario.html", {"beneficiario": beneficiario})
+
+def update_beneficiario(request, id):
+    beneficiario = get_object_or_404(Beneficiario, id=id)
+    if request.method == "POST":
+        beneficiario.nome = request.POST.get("nome")
+        beneficiario.apelido = request.POST.get("apelido")
+        beneficiario.documento = request.POST.get("documento")
+        beneficiario.endereco = request.POST.get("endereco")
+        beneficiario.telefone = request.POST.get("telefone")
+        beneficiario.observacoes = request.POST.get("observacoes")
+        beneficiario.save()
+        return redirect(lista_beneficiarios)
+
+def delete_beneficiario(request, id):
+    beneficiario = get_object_or_404(Beneficiario, id=id)
+    beneficiario.delete()
+    return redirect(lista_beneficiarios)
