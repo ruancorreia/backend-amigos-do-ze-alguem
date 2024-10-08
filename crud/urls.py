5d3f36f276963649ab67e67f3e3bb9068ce275df
Bernardo Cezar <b.cezar1812@gmail.com>
2024-10-08 15:18:33 -0300

    Criação da classe Voluntario, criação das urls do CRUD relacionado aos voluntarios, adição de novos templates e views dos voluntarios

--- crud/urls.py
+++ crud/urls.py
@@ -3,7 +3,8 @@ from django.urls import path, include
 from .views import (
     home, salvar, editar, update, delete,
     lista_beneficiarios, salvar_beneficiario, editar_beneficiario,
-    update_beneficiario, delete_beneficiario
+    update_beneficiario, delete_beneficiario, lista_voluntarios, salvar_voluntario, editar_voluntario,
+    update_voluntario, delete_voluntario
 )
 
 urlpatterns = [
@@ -12,13 +13,24 @@ urlpatterns = [
     path('editar/<int:id>', editar, name='editar'),
     path('update/<int:id>', update, name='update'),
     path('delete/<int:id>', delete, name='delete'),
-    
-# Beneficiarios
+
+    # Beneficiarios
     path('beneficiarios/', lista_beneficiarios, name='lista_beneficiarios'),
     path('beneficiarios/salvar/', salvar_beneficiario, name='salvar_beneficiario'),
-    path('beneficiarios/editar/<int:id>/', editar_beneficiario, name='editar_beneficiario'),
-    path('beneficiarios/update/<int:id>/', update_beneficiario, name='update_beneficiario'),
-    path('beneficiarios/delete/<int:id>/', delete_beneficiario, name='delete_beneficiario'),
+    path('beneficiarios/editar/<int:id>/',
+         editar_beneficiario, name='editar_beneficiario'),
+    path('beneficiarios/update/<int:id>/',
+         update_beneficiario, name='update_beneficiario'),
+    path('beneficiarios/delete/<int:id>/',
+         delete_beneficiario, name='delete_beneficiario'),
+
+    # Voluntários
+    path('voluntarios/', lista_voluntarios, name='lista_voluntarios'),
+    path('voluntarios/salvar/', salvar_voluntario, name='salvar_voluntario'),
+    path('voluntarios/editar/<int:id>/',
+         editar_voluntario, name='editar_voluntario'),
+    path('voluntarios/update/<int:id>/',
+         update_voluntario, name='update_voluntario'),
+    path('voluntarios/delete/<int:id>/',
+         delete_voluntario, name='delete_voluntario'),
 ]
-
-
--- crud/views.py
+++ crud/views.py
@@ -1,9 +1,11 @@
 from django.shortcuts import render, redirect, get_object_or_404
-from .models import Pessoa, Beneficiario
+from .models import Pessoa, Beneficiario, Voluntario
+
 
 def home(request):
     pessoas = Pessoa.objects.all
     return render(request, "index.html", {"pessoas": pessoas})
+
 
 def salvar(request):
     vnome = request.POST.get("nome")
@@ -11,9 +13,11 @@ def salvar(request):
     pessoas = Pessoa.objects.all()
     return render(request, "index.html", {"pessoas": pessoas})
 
+
 def editar(request, id):
     pessoa = Pessoa.objects.get(id=id)
     return render(request, "update.html", {"pessoa": pessoa})
+
 
 def update(request, id):
     vnome = request.POST.get("nome")
@@ -22,15 +26,19 @@ def update(request, id):
     pessoa.save()
     return redirect(home)
 
+
 def delete(request, id):
     pessoa = Pessoa.objects.get(id=id)
     pessoa.delete()
     return redirect(home)
 
-# beneficiarios
+# beneficiarios ------------------------------------------------------------------------
+
+
 def lista_beneficiarios(request):
     beneficiarios = Beneficiario.objects.all()
     return render(request, "lista_beneficiarios.html", {"beneficiarios": beneficiarios})
+
 
 def salvar_beneficiario(request):
     if request.method == "POST":
@@ -40,13 +48,16 @@ def salvar_beneficiario(request):
         endereco = request.POST.get("endereco")
         telefone = request.POST.get("telefone")
         observacoes = request.POST.get("observacoes")
-        Beneficiario.objects.create(nome=nome, apelido=apelido, documento=documento, endereco=endereco, telefone=telefone, observacoes=observacoes)
+        Beneficiario.objects.create(nome=nome, apelido=apelido, documento=documento,
+                                    endereco=endereco, telefone=telefone, observacoes=observacoes)
         return redirect(lista_beneficiarios)
     return render(request, "form_beneficiario.html")
+
 
 def editar_beneficiario(request, id):
     beneficiario = get_object_or_404(Beneficiario, id=id)
     return render(request, "update_beneficiario.html", {"beneficiario": beneficiario})
+
 
 def update_beneficiario(request, id):
     beneficiario = get_object_or_404(Beneficiario, id=id)
@@ -60,7 +71,46 @@ def update_beneficiario(request, id):
         beneficiario.save()
         return redirect(lista_beneficiarios)
 
+
 def delete_beneficiario(request, id):
     beneficiario = get_object_or_404(Beneficiario, id=id)
     beneficiario.delete()
     return redirect(lista_beneficiarios)
+
+# Voluntarios ------------------------------------------------------------------------
+
+
+def lista_voluntarios(request):
+    voluntarios = Voluntario.objects.all()
+    return render(request, "lista_voluntarios.html", {"voluntarios": voluntarios})
+
+
+def salvar_voluntario(request):
+    if request.method == "POST":
+        nome = request.POST.get("nome")
+        cpf = request.POST.get("cpf")
+        telefone = request.POST.get("telefone")
+        Voluntario.objects.create(nome=nome, cpf=cpf, telefone=telefone)
+        return redirect(lista_voluntarios)
+    return render(request, "form_voluntario.html")
+
+
+def editar_voluntario(request, id):
+    voluntario = get_object_or_404(Voluntario, id=id)
+    return render(request, "update_voluntario.html", {"voluntario": voluntario})
+
+
+def update_voluntario(request, id):
+    voluntario = get_object_or_404(Voluntario, id=id)
+    if request.method == "POST":
+        voluntario.nome = request.POST.get("nome")
+        voluntario.cpf = request.POST.get("cpf")
+        voluntario.telefone = request.POST.get("telefone")
+        voluntario.save()
+        return redirect(lista_voluntarios)
+
+
+def delete_voluntario(request, id):
+    voluntario = get_object_or_404(Voluntario, id=id)
+    voluntario.delete()
+    return redirect(lista_voluntarios)
